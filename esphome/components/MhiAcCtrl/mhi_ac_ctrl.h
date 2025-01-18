hberntsen/mhi-ac-ctrl-esp32-c3

commit 312ff23567241c6097b11e94b0dcdaf102ec44b3

Replace config setters with config struct

esphome/components/MhiAcCtrl/mhi_ac_ctrl.h
@@ -221,21 +221,9 @@ public:
 class MhiAcCtrl : public climate::Climate,
                   public Component {
 public:
-    void set_mosi_pin(InternalGPIOPin *pin) {
-        this->ac_config_.mosi = static_cast<gpio_num_t>(pin->get_pin());
-    }
-    void set_miso_pin(InternalGPIOPin *pin) {
-        this->ac_config_.miso = static_cast<gpio_num_t>(pin->get_pin());
-    }
-    void set_sclk_pin(InternalGPIOPin *pin) {
-        this->ac_config_.sclk = static_cast<gpio_num_t>(pin->get_pin());
-    }
-    void set_cs_in_pin(InternalGPIOPin *pin) {
-        this->ac_config_.cs_in = static_cast<gpio_num_t>(pin->get_pin());
-    }
-    void set_cs_out_pin(InternalGPIOPin *pin) {
-        this->ac_config_.cs_out = static_cast<gpio_num_t>(pin->get_pin());
-    }
+  MhiAcCtrl(const mhi_ac::Config &config) {
+    this->ac_config_ = config;
+  }
 
     void setup() override
     {

esphome/components/MhiAcCtrl/MHI-AC-Ctrl-core.cpp
@@ -516,13 +516,13 @@ static void mhi_poll_task(void *arg)
 void init(const Config& config) {
     esp_err_t err;
 
-    gpio_cs_out = config.cs_out;
+    gpio_cs_out = static_cast<gpio_num_t>(config.cs_out_pin);
 
     // configuration for the SPI bus
     spi_bus_config_t buscfg = {
-        .mosi_io_num = config.mosi,
-        .miso_io_num = config.miso,
-        .sclk_io_num = config.sclk,
+        .mosi_io_num = config.mosi_pin,
+        .miso_io_num = config.miso_pin,
+        .sclk_io_num = config.sclk_pin,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
         .data4_io_num = -1,
@@ -536,7 +536,7 @@ void init(const Config& config) {
 
     // configuration for the SPI slave interface
     spi_slave_interface_config_t slvcfg = {
-        .spics_io_num = config.cs_in,
+        .spics_io_num = config.cs_in_pin,
         .flags = SPI_SLAVE_BIT_LSBFIRST,
         .queue_size = 1,
         .mode = 3,                    //CPOL=1, CPHA=1
@@ -576,7 +576,7 @@ void init(const Config& config) {
 
     gpio_config_t io_conf = {};
     io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = (1ULL<<config.sclk);
+    io_conf.pin_bit_mask = (1ULL<<config.sclk_pin);
 io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.pull_up_en = GPIO_PULLUP_ENABLE;                                     // required to prevent abort() caused by floating pin when daughtboard not connected
     io_conf.intr_type = GPIO_INTR_LOW_LEVEL;                    // when this is set to NEGEDGE, DMA sometimes doesn't read the last 4 bytes
@@ -585,8 +585,8 @@ io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     gpio_config(&io_conf);
 
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    gpio_isr_handler_add(config.sclk, gpio_isr_handler, NULL);
-    gpio_intr_enable(config.sclk);
+    gpio_isr_handler_add(static_cast<gpio_num_t>(config.sclk_pin), gpio_isr_handler, NULL);
+    gpio_intr_enable(static_cast<gpio_num_t>(config.sclk_pin));
 
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pin_bit_mask = (1ULL<<gpio_cs_out);

esphome/components/MhiAcCtrl/MHI-AC-Ctrl-core.h
@@ -48,11 +48,12 @@
 
 namespace mhi_ac {
 struct Config {
-  gpio_num_t mosi;
-  gpio_num_t miso;
-  gpio_num_t sclk;
-  gpio_num_t cs_in;
-  gpio_num_t cs_out;
+  bool use_long_frame;
+  uint8_t mosi_pin;
+  uint8_t miso_pin;
+  uint8_t sclk_pin;
+  uint8_t cs_in_pin;
+  uint8_t cs_out_pin;
 };
 
 enum class ACPower {

esphome/components/MhiAcCtrl/__init__.py
@@ -39,21 +39,14 @@ CONFIG_SCHEMA = cv.Schema(
 ).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    config_struct = cg.StructInitializer(
+        ConfigStruct,
+        ("mosi_pin", (await cg.gpio_pin_expression(config[CONF_MOSI_PIN])).get_pin()),
+        ("miso_pin", (await cg.gpio_pin_expression(config[CONF_MISO_PIN])).get_pin()),
+        ("sclk_pin", (await cg.gpio_pin_expression(config[CONF_SCLK_PIN])).get_pin()),
+        ("cs_in_pin", (await cg.gpio_pin_expression(config[CONF_CS_IN_PIN])).get_pin()),
+        ("cs_out_pin", (await cg.gpio_pin_expression(config[CONF_CS_OUT_PIN])).get_pin()),
+    )
 
-    mosi_pin = await cg.gpio_pin_expression(config[CONF_MOSI_PIN])
-    cg.add(var.set_mosi_pin(mosi_pin))
-
-    miso_pin = await cg.gpio_pin_expression(config[CONF_MISO_PIN])
-    cg.add(var.set_miso_pin(miso_pin))
-
-    sclk_pin = await cg.gpio_pin_expression(config[CONF_SCLK_PIN])
-    cg.add(var.set_sclk_pin(sclk_pin))
-
-    cs_in_pin = await cg.gpio_pin_expression(config[CONF_CS_IN_PIN])
-    cg.add(var.set_cs_in_pin(cs_in_pin))
-
-    cs_out_pin = await cg.gpio_pin_expression(config[CONF_CS_OUT_PIN])
-    cg.add(var.set_cs_out_pin(cs_out_pin))
-
+    var = cg.new_Pvariable(config[CONF_ID], config_struct)
     return await cg.register_component(var, config)

commit 9ad5395f60a9ef53e5e3e6155af323e62cb7ba3b

Initialise not used sensors to 0
For #13. Fixes the issue introduced in 312ff23 ("Replace config setters
with config struct") where the unused sensors were not being initialised
any more after a constructor was defined.

esphome/components/MhiAcCtrl/mhi_ac_ctrl.h
@@ -454,12 +454,12 @@ protected:
 
 protected:
 #ifdef USE_SELECT
-    MhiVanesUD *vanes_ud_select_;
-    MhiVanesLR *vanes_lr_select_;
+    MhiVanesUD *vanes_ud_select_ = nullptr;
+    MhiVanesLR *vanes_lr_select_ = nullptr;
 #endif
-    MhiFrameErrors *frame_errors_sensor_;
-    MhiTotalEnergy *total_energy_sensor_;
-    MhiPower *power_sensor_;
+    MhiFrameErrors *frame_errors_sensor_ = nullptr;
+    MhiTotalEnergy *total_energy_sensor_ = nullptr;
+    MhiPower *power_sensor_ = nullptr;
     mhi_ac::Config ac_config_;
 
 public: