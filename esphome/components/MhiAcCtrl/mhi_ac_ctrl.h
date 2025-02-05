b49f7416ff51ac143095e4e203318c3bb42e25b3
Harm Berntsen <git@harmberntsen.nl>
2025-02-05 18:23:28 +0100

    Report current temperature with 1 decimal
    The hardware provides it in steps of 0.25. Home assistant will now round
    it to 1 decimal instead of 0. This can be reverted by specifying the
    step size in the yaml:
    
    climate:
      - platform: MhiAcCtrl
        visual:
          temperature_step:
            target_temperature: 1.0
            current_temperature: 1.0

--- esphome/components/MhiAcCtrl/mhi_ac_ctrl.h
+++ esphome/components/MhiAcCtrl/mhi_ac_ctrl.h
@@ -518,6 +518,7 @@ protected:
         traits.set_visual_min_temperature(this->minimum_temperature_);
         traits.set_visual_max_temperature(this->maximum_temperature_);
         traits.set_visual_temperature_step(this->temperature_step_);
+        traits.set_visual_current_temperature_step(0.25);
         traits.add_supported_custom_fan_mode(custom_fan_ultra_low);
         traits.set_supported_fan_modes({ CLIMATE_FAN_LOW, CLIMATE_FAN_MEDIUM, CLIMATE_FAN_HIGH, CLIMATE_FAN_AUTO });
         //traits.set_supported_swing_modes({ CLIMATE_SWING_VERTICAL });
